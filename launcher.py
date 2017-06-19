28ea67c2a09b04b70755b03c8e49fd8ec43d9cc6
alclsTit <c936891@gmail.com>
2017-06-19 18:29:46 +0900

    기타 수정분
    c++연동을 하려다 버그가 생겼습니다.  기타 함수들 수정

--- launcher.py
+++ launcher.py
@@ -18,8 +18,10 @@ regKey = 'XhKqnYiL44B3YdVVzKn2K2HUJ0tJJMUAAveunEp5YXfcfJhkpnUmo98E%2FlRE1X5CjqWT
 import tkinter.messagebox
 
 RenderText = None
-DataList = []
+DepRenderText = None
 g_Tk = None
+showList = []
+
 
 def InitTopText():
     TempFont = font.Font(g_Tk, size =20, weight = 'bold', family = 'Consolas')
@@ -51,13 +53,6 @@ def InitSearchListBox():
     SearchListBox.pack()
     SearchListBox.place(x=10, y=60)
 
-
-
-
-
-
-
-
 def InitInputLabel():
     global InputLabel
     TempFont = font.Font(g_Tk, size=15, weight='bold', family = 'Consolas')
@@ -81,17 +76,31 @@ def InitRenderText():
     RenderTextScrollbar.pack(side=RIGHT, fill=BOTH)
     RenderText.configure(state='disabled')
 
+def InitDepInfoRenderText():
+    global DepRenderText
+    TempFont = font.Font(g_Tk, size=10, family='Consolas')
+    DepRenderText = Text(g_Tk, width=50, height=40, borderwidth=12, relief='ridge')
+    DepRenderText.pack()
+    DepRenderText.place(x=600, y=130)
+
+    DepRenderText.configure(state='disabled')
+
 def InitSearchButton():
     TempFont = font.Font(g_Tk, size=16, weight='bold', family='Consolas')
 
     SearchButton = Button(g_Tk, font=TempFont, text="검색", command=SearchButtonAction)
-    gmailButton = Button(g_Tk,font= TempFont, text="이메일 보내기", command = OpenMailWindow)
+    #gmailButton = Button(g_Tk,font= TempFont, text="이메일 보내기", command = OpenMailWindow)
+    #gmailButton = Button(g_Tk, font=TempFont, text="시발 보내기", command=SearchshowBoxAction)
 
-    gmailButton.pack()
+    shootMailButton = Button(g_Tk, font=TempFont, text="이메일 보내기", command=SendGmail)
+
+    #gmailButton.pack()
     SearchButton.pack()
+    shootMailButton.pack()
 
     SearchButton.place(x=730, y = 65)
-    gmailButton.place(x=810, y = 65)
+    #gmailButton.place(x=810, y = 45)
+    shootMailButton.place(x = 810, y = 90)
 
 #-----------------------------------------------------------------------------------------------------------
 #이미지
@@ -100,27 +109,35 @@ def MakeImage(ImageName):
      #root = Tk()
      #root.geometry("1024x1024+500+200")
 
-     url = str(ImageItemList[0][3])
+     url = str(ImageItemList[2][3])
+
      with urllib.request.urlopen(url) as u:
          raw_data = u.read()
 
      im = Image.open(BytesIO(raw_data))
      image = ImageTk.PhotoImage(im)
 
-     #Label()
-     #ImageHeight = ImageItemList[0][1]
-     #ImageWidth = ImageItemList[0][0]
-
      label = Label(g_Tk, image = image, height=400, width=400)
      label.pack()
-     label.place(x=0, y=0)
-
-     g_Tk.mainloop()
+     label.place(x=300, y=0)
 
 #-----------------------------------------------------------------------------------------------------------
 
+def SendGmail():
+    global showBox, showList
+    # 0 / 3 / 6 / 9 / 12
+    SearchIndex = showBox.curselection()[0]
+    Label = SearchIndex // 3
 
+    MailList = []
+    for i in range(len(showList)):
+        if showList[i][0] == Label:
+            tempList = [showList[i][0], showList[i][1], showList[i][2], showList[i][3], showList[i][4], showList[i][5],
+                        showList[i][6],
+                        showList[i][7], showList[i][8]]
+            MailList.append(tempList)
 
+    sendMain(MailList)
 
 
 #-----------------------------------------------------------------------------------------------------------
@@ -167,9 +184,7 @@ def MailTk():
 def OpenMailWindow():
     MailTk()
     #sendMain()
-    
-def SendGmail():
-    RenderText.insert(INSERT, "이메일이쟝")
+
 
 
 #-----------------------------------------------------------------------------------------------------------
@@ -189,7 +204,6 @@ def SearchButtonAction():
        SearchInfoAddrAndName()
    elif SearchIndex == 3:
        MakeImage(InputLabel.get())
-
 
    RenderText.configure(state='disabled')
 
@@ -212,7 +226,7 @@ def SearchInfoAddrAndName():
 
     req = conn.getresponse()
     if int(req.status) == 200:  # okay
-        return ExtractDataFromURI(req.read())
+        return temp(req.read())
     else:
         print("OpenAPI request has been failed!! please retry")
         return None
@@ -231,7 +245,7 @@ def SearchInfoName():
 
     req = conn.getresponse()
     if int(req.status) == 200:  # okay
-        return ExtractDataFromURI(req.read())
+        return temp(req.read())
     else:
         print("OpenAPI request has been failed!! please retry")
         return None
@@ -261,7 +275,7 @@ def temp(strXml):
     from xml.etree import ElementTree
     tree = ElementTree.fromstring(strXml)
 
-    global itemElements , showBox
+    global itemElements , showBox , showList
     # global SList
 
     itemElements = tree.getiterator("item")  # return list type
@@ -280,7 +294,16 @@ def temp(strXml):
                             width=0, height=27, borderwidth=12, relief='ridge'
                             , yscrollcommand=ListBoxScrollbar.set)
     index = 0
+    #tempList = []
+    #if len(tempList) > 0:
+    #    tempList.clear()
+
     for item in itemElements:
+        tempList = []
+
+        #if len(tempList) > 0:
+        #    tempList.clear()
+
         BigAdress = item.find("dutyAddr")  # 도
         # SmallAdress = item.find("Q1")  # 구
         QZ = item.find("dutyName")  # B : 병원 C : 의원
@@ -288,32 +311,115 @@ def temp(strXml):
         Tel = item.find("dutyTel1")  # 일하는날
         JobDef = item.find("dutyInf")
 
+        tempList += [cnt,QZ.text,BigAdress.text,Tel.text,Emergency.text]
+
+        weekdaySTime = item.find("dutyTime1s")  # 평일 시작시간
+        weekdayETime = item.find("dutyTime1c")  # 평일 끝나는시간
+
+        weekendSTime = item.find("dutyTime6s")  # 토요일 진료 시작시간
+        weekendETime = item.find("dutyTime6c")  # 토요일 진료 끝나는시간
+
+        weekendSTimeH = item.find("dutyTime7s")  # 일요일 진료 시작시간
+        weekendETimeH = item.find("dutyTime7c")  # 일요일 진료 끝나는시간
+
+        HolidaySTime = item.find("dutyTime8s")  # 공휴일 진료 시작시간
+        HolidayETime = item.find("dutyTime8c")  # 공휴일 진료 끝나는시간
+
+        if weekdaySTime == None or weekdayETime == None:
+            pass
+        else:
+            tempList += [weekdaySTime.text,weekdayETime.text]
+
+        if weekendSTime == None or weekendETime == None:
+            pass
+        else:
+            tempList += [weekendSTime.text,weekendETime.text]
+
+        if weekendSTimeH == None or weekendETimeH == None:
+            pass
+        else:
+            tempList += [weekendSTimeH.text, weekendETimeH.text]
+
+        if HolidaySTime == None or HolidayETime == None:
+            pass
+        else:
+            tempList += [HolidaySTime.text, HolidayETime.text]
+
+        if JobDef != None:
+            tempList += [JobDef.text]
+        else:
+            pass
+
+        showList.append(tempList)
+
         showBox.insert(index, "주소: " + BigAdress.text)
         showBox.insert(index, "전화번호: " + Tel.text)
         showBox.insert(index,"[" + str(cnt + 1) + "]" + "병원이름: " + QZ.text)
 
-
-
-
-
         cnt  = cnt + 1
-        if cnt >= 3:
+
+        if cnt >= 10:
             break
-        #RenderText.insert(INSERT, "[")
-        #RenderText.insert(INSERT, cnt + 1)
-        #RenderText.insert(INSERT, "]")
-        #RenderText.insert(INSERT, "병원이름: ")
-        #RenderText.insert(INSERT, QZ.text)
-        #RenderText.insert(INSERT, "굈")
-        #RenderText.insert(INSERT, "주소: ")
-        #RenderText.insert(INSERT, BigAdress.text)
-        #RenderText.insert(INSERT, "굈")
-        #RenderText.insert(INSERT, "전화번호: ")
-        #RenderText.insert(INSERT, Tel.text)
-        #RenderText.insert(INSERT, "굈")
-        #RenderText.insert(INSERT, "응급실 운영여부: ")
+
     showBox.pack()
     showBox.place(x = 10,y = 130)
+    SearchshowBoxAction()
+
+def SearchshowBoxAction():
+    global showBox , showList , MailList ,DepRenderText
+    #0 / 3 / 6 / 9 / 12
+    SearchIndex = showBox.curselection()[0]
+    Label = SearchIndex // 3
+
+    MailList = []
+    for i in range(len(showList)):
+        if showList[i][0] == Label:
+            tempList = [showList[i][0],showList[i][1],showList[i][2],showList[i][3],showList[i][4],showList[i][5],showList[i][6],
+                        showList[i][7],showList[i][8]]
+            MailList.append(tempList)
+
+    #MakeImage(MailList[0][1])
+
+    DepRenderText.insert(INSERT, "병원이름: ")
+    DepRenderText.insert(INSERT, MailList[0][1])
+    DepRenderText.insert(INSERT, "굈")
+    DepRenderText.insert(INSERT, "주소: ")
+    DepRenderText.insert(INSERT, MailList[0][2])
+    DepRenderText.insert(INSERT, "굈")
+    DepRenderText.insert(INSERT, "전화번호: ")
+    DepRenderText.insert(INSERT, MailList[0][3])
+    DepRenderText.insert(INSERT, "굈")
+
+    if (MailList[0][4] == "1"):
+        RenderText.insert(INSERT, "응급실 운영")
+    else:
+        RenderText.insert(INSERT, "응급실 운영 안함")
+        RenderText.insert(INSERT, "굈")
+
+    RenderText.insert(INSERT, "평일 진료 시작시간:")
+    RenderText.insert(INSERT, MailList[0][5])
+    RenderText.insert(INSERT, "평일 진료 종료시간:")
+    RenderText.insert(INSERT, MailList[0][5])
+    RenderText.insert(INSERT, "굈")
+
+    RenderText.insert(INSERT, "토요일 진료 시작시간:")
+    RenderText.insert(INSERT, MailList[0][6])
+    RenderText.insert(INSERT, "토요일 진료 종료시간:")
+    RenderText.insert(INSERT, MailList[0][6])
+    RenderText.insert(INSERT, "굈")
+
+    RenderText.insert(INSERT, "일요일 진료 시작시간:")
+    RenderText.insert(INSERT, MailList[0][7])
+    RenderText.insert(INSERT, "일요일 진료 종료시간:")
+    RenderText.insert(INSERT, MailList[0][7])
+    RenderText.insert(INSERT, "굈")
+
+    RenderText.insert(INSERT, "공휴일 진료 시작시간:")
+    RenderText.insert(INSERT, MailList[0][8])
+    RenderText.insert(INSERT, "공휴일 진료 종료시간:")
+    RenderText.insert(INSERT, MailList[0][8])
+    RenderText.insert(INSERT, "굈")
+
 
 def ExtractDataFromURI(strXml):
     from xml.etree import ElementTree
@@ -484,11 +590,13 @@ def Main():
     InitInputLabel()
     InitSearchButton()
     InitRenderText()
+    #InitDepInfoRenderText()
+    # DepRenderText()
     g_Tk.mainloop()
 
 
 def ShowLoop():
-    #Title()
+    Title()
     Main()
 
 
